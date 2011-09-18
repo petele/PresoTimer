9aa52cd266263be31d21631965ef685fa6995f31
Pete LePage <pete.lepage@pobox.com>
2011-09-18 16:34:13 -0300

    First Real Checkin

---
+++ config.js
@@ -0,0 +1,39 @@
+window.slidesConfig = window.slidesConfig || {
+  // Slide settings
+  settings : {
+    useBuilds: true,
+    useGDDBranding: true,
+    showCWS: true,
+    showIAP: false
+
+  },
+  info: {
+    // Personal info
+    name: 'Pete LePage',
+    pic: 'http://petelepage.com/blog/wp-content/uploads/2011/09/petele.jpg',
+    gplus: 'https://bit.ly/petele-plus',
+    twitter: 'petele',
+    blog: 'http://petelepage.com/blog/',
+
+    slides: 'http://bit.ly/qQxz1q',
+    feedback: '',
+    djbpDemo: 'http://localhost:5103/',
+    djbpSrc: 'http://bit.ly/quyPMZ',
+    ioreaderDemo: 'http://bit.ly/pgGcbN',
+    ioreaderSrc: 'http://bit.ly/nlWNrZ',
+
+    // GDD info
+    location: 'S&atilde;o Paulo, Brazil',
+    country: 'BR',
+    date: new Date('Sept 17 2011 15:46'),
+    logo: 'http://www.google.com/events/developerday/2011/static/img/event-sao-paulo.png'
+  }
+};
+
+window.timerConfig = window.timerConfig || {
+  settings : {
+    date: new Date('Sept 18 2011 16:22'),
+    minutes: 2,
+    warnAt: 1
+  }
+};
